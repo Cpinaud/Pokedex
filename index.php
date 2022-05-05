9f4b58a7c32000f59b357ede1a0b1bf9a8676cda
cpinaud <cintiapinaud@gmail.com>
2022-05-04 22:49:00 -0300

    Conexion a bd

--- index.php
+++ index.php
@@ -7,6 +7,18 @@
     <title>Pokedex</title>
 </head>
 <body>
-    
+    <div class="contenido">
+        <form action="login.php" method="POST" enctype="multipart/form-data">
+            <div>
+                <label for="usuario">User: </label>
+                <input type="text" name="usuario" id="usuario">
+            </div>
+            <div>
+                <label for="clave">Password: </label>
+                <input type="password" name="clave" id="clave">
+            </div>
+            <input type="submit" name="enviar" value='ENVIAR'>
+        </form>
+    </div>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,62 @@
+<?php
+    $usuario = $_POST["usuario"];
+    $password = $_POST["clave"];
+    $enviado = $_POST["enviar"];
+    if($enviado){
+
+    
+        $config = parse_ini_file("config.ini");
+        
+        $conexion =mysqli_connect($config["host"],$config["usuario"],$config["clave"],$config["base"]);
+
+        if (!$conexion) {
+            die("Connection failed: " . mysqli_connect_error());
+        }else{
+            $sql = "SELECT * FROM tipos_pokemon ";
+            $comando = $conexion->prepare($sql);
+            $password = md5($password);
+
+            
+            $comando->execute();
+            $resultado = $comando->get_result();
+            $fila = $resultado->fetch_assoc();
+    
+            var_dump($fila);
+    
+            $conexion->close();
+
+        }
+        
+        
+
+
+
+        
+    }
+    ?>
+      
+    
+
+<!--
+    
+
+
+       
+
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+</head>
+<body>
+<form action="prueba.php" method="post">
+    <input type="text" name="usuario" value=""><br>
+    <input type="password" name="clave" value=""><br>
+    <input type="submit">
+
+
+</form>
+</body>
+</html>-->
